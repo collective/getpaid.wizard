9bfe6586af564656d8749ca4d7e2c2e22ef92022
Paul J Stevens <paul@nfg.nl>
2013-07-03 11:35:54 +0000

    pep8

--- src/getpaid/wizard/__init__.py
+++ src/getpaid/wizard/__init__.py
@@ -1,4 +1,8 @@
-# $Id: $
-# 
+#
 from _wizard import Wizard, ControllerBase, ViewControllerBase, \
                     ListViewController, DataManager
+
+__all__ = [Wizard, ControllerBase, ViewControllerBase,
+           ListViewController, DataManager]
+
+#EOF
