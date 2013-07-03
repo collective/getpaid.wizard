c4bce92dcd58ce563bc8713878c6903bf08e514b
Paul J Stevens <paul@nfg.nl>
2013-07-03 11:42:27 +0000

    fix small regression

--- src/getpaid/wizard/__init__.py
+++ src/getpaid/wizard/__init__.py
@@ -2,7 +2,5 @@
 from _wizard import Wizard, ControllerBase, ViewControllerBase, \
                     ListViewController, DataManager
 
-__all__ = [Wizard, ControllerBase, ViewControllerBase,
-           ListViewController, DataManager]
 
 #EOF
